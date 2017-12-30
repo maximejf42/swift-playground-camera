cc210a6baa1b87d1b37763f546a89a485a7be0ea
jlsa <jlsahoekstra@gmail.com>
2017-12-31 00:00:37 +0100

    updated to Swift 4

--- Camera.playground/Contents.swift
+++ Camera.playground/Contents.swift
@@ -8,14 +8,14 @@ let view = NSView(frame: NSRect(x: 0.0, y: 0.0, width: 640.0, height: 480.0))
 
 let session = AVCaptureSession()
 
-session.sessionPreset = AVCaptureSessionPreset640x480
+session.sessionPreset = AVCaptureSession.Preset.vga640x480
 session.beginConfiguration()
 session.commitConfiguration()
 
 var input : AVCaptureDeviceInput
 if let devices : [AVCaptureDevice] = AVCaptureDevice.devices() as? [AVCaptureDevice] {
     for device in devices {
-        if device.hasMediaType(AVMediaTypeVideo) && device.supportsAVCaptureSessionPreset(AVCaptureSessionPreset640x480) {
+        if device.hasMediaType(AVMediaType.video) && device.supportsSessionPreset(AVCaptureSession.Preset.vga640x480) {
             do {
                 input = try AVCaptureDeviceInput(device: device as AVCaptureDevice) as AVCaptureDeviceInput
                 
@@ -31,7 +31,7 @@ if let devices : [AVCaptureDevice] = AVCaptureDevice.devices() as? [AVCaptureDev
     }
     
     let output = AVCaptureVideoDataOutput()
-    output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable: Int(kCVPixelFormatType_32BGRA)]
+    output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable as! String: Int(kCVPixelFormatType_32BGRA)]
     output.alwaysDiscardsLateVideoFrames = true
     
     if session.canAddOutput(output) {
@@ -48,3 +48,4 @@ if let devices : [AVCaptureDevice] = AVCaptureDevice.devices() as? [AVCaptureDev
     PlaygroundPage.current.liveView = view
     
 }
+
